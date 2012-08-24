2dfa01e0537ae39c3e2f6c052e4852fc35ad953a
Goon <matt@lnam.net>
2012-08-24 16:57:05 -0500

    added some comments for how the filter works

--- js/script.js
+++ js/script.js
@@ -20,14 +20,20 @@ function strictNewWindow() {
 }
 
 function connexion() {
-  $('.somePositions').hide();
-  $('.noPositions').hide();
-  $('.connecting').hide();
-  $('select').change(function() {
-    var gradValue = $('#gradDate :selected').val();
-    var majorValue = $('#major :selected').val();
-    bothValues = '.' + gradValue + '.' + majorValue;
+
+  // A little preliminary work
+  $('.somePositions').hide(); // here we hide the congratulations text that is displayed after a match is made
+  $('.noPositions').hide(); // here we hide the sorry message that is displayed if there is no match
+  $('.connecting').hide(); // this hides the connecting button which is shown for a couple seconds while a match is being formulated
+
+  // User selects their info and we save the values in variables
+  $('select').change(function() { // change triggers when the user changes the selected item
+    var gradValue = $('#gradDate :selected').val(); // we set a variable called gradValue equal to the value of the option selected in the select element
+    var majorValue = $('#major :selected').val(); // we do the same for the Major select element
+    bothValues = '.' + gradValue + '.' + majorValue; // we then concatenate these values and place it in a new variable called bothValues
   });
+
+  // This is where we output a match, or if there is no match, a sorry message
   $('.connectMe').click(function() {
       $('body,html').animate({
 				scrollTop: 500
